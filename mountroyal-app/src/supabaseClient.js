3643292a1f83babc1bf3117d821ba9666ff64298
Jeremiahtec <aderintojeremiah257@gmail.com>
2026-07-28 17:06:04 +0100

    Added DELETE route for tenants (refined again)

--- mountroyal-app/src/supabaseClient.js
+++ mountroyal-app/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://nrpwaovxkshmukuowyrv.supabase.co/rest/v1/';
+const supabaseUrl = 'https://nrpwaovxkshmukuowyrv.supabase.co';
 const supabaseAnonKey = 'sb_publishable_xOvxuMLpF3_5pfZuZArdkQ_p9j7Em8i';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

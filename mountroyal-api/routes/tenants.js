d1510a8fccf3a7e91940d5bc1dc21033c3505a01
Jeremiahtec <aderintojeremiah257@gmail.com>
2026-07-28 14:39:03 +0100

    Added DELETE route for tenants (refined)

--- mountroyal-api/routes/tenants.js
+++ mountroyal-api/routes/tenants.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db'); // Adjust this path if your db.js is located somewhere else
+const pool = require('../config/db'); 
 
 // 1. GET ALL ACTIVE TENANTS
 router.get('/', async (req, res) => {
